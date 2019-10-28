08163827ad79c308a1ee2481dceb4426d99abdd6
ppvalluri09 <ppvalluri09@outlook.com>
2019-10-29 03:18:40 +0530

    Made sensor file

---
+++ Itsy.ino
@@ -0,0 +1,11 @@
+#include"sensor.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Sensor s1 = Sensor(13, 12);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ sensor.h
@@ -0,0 +1,45 @@
+using namespace std;
+
+class Sensor {
+	private:
+		unsigned int TRIG_PIN;
+		unsigned int ECHO_PIN;
+
+	public:
+		Sensor() {
+			this->TRIG_PIN = 13;
+			this->ECHO_PIN = 12;
+			pinMode(this->TRIG_PIN, OUTPUT);
+			pinMode(this->ECHO_PIN, INPUT);
+		}
+
+		Sensor(const unsigned int TRIG_PIN, const unsigned int ECHO_PIN) {
+			this->TRIG_PIN = TRIG_PIN;
+			this->ECHO_PIN = ECHO_PIN;
+			pinMode(this->TRIG_PIN, OUTPUT);
+			pinMode(this->ECHO_PIN, INPUT);
+		}
+
+		int getDistance();
+		unsigned int getDuration();
+		int getAngle(int distance);
+};
+
+
+unsigned int Sensor::getDuration() {
+	digitalWrite(this->TRIG_PIN, LOW);
+	delayMicroseconds(2);
+	digitalWrite(this->TRIG_PIN, HIGH);
+	delayMicroseconds(2);
+	digitalWrite(this->TRIG_PIN, LOW);
+	delayMicroseconds(2);
+
+	unsigned int duration = pulseIn(this->ECHO_PIN, HIGH);
+	return duration;
+}
+
+int Sensor::getDistance() {
+	unsigned int duration = this->getDuration();
+	int distance = duration / 29 / 2;
+	return distance;
+}
